2d5888415f0a6bd735ffb89bcd90b65203bbf6fe
voidrlm <varjun609@gmail.com>
2022-11-26 00:13:57 +0530

    Changes in thrash view

---
+++ src/views/thrash.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container fluid>
+    <v-card-title
+      class="mb-n10"
+      :class="
+        $vuetify.breakpoint.mdAndUp ? 'text-h4 mt-n7' : 'text-h5 justify-center'
+      "
+    >
+      <v-icon size="35" class="mr-2">mdi-delete-empty</v-icon>Thrash
+    </v-card-title>
+    <dataTable
+      :data="
+        database.filter(function (mail) {
+          return mail.isThrash;
+        })
+      "
+      :tableTitle="'Thrash'"
+    />
+  </v-container>
+</template>
+<script>
+import dataTable from "../components/table/dataTable.vue";
+import { database } from "../resources/mailDatabase";
+export default {
+  name: "inbox-component",
+  data: () => ({ database }),
+  components: {
+    dataTable,
+  },
+
+  computed: {},
+};
+</script>
