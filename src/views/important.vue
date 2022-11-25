3f8d44cc6658adfc1b2d825fae2d57f9171e0ddd
voidrlm <varjun609@gmail.com>
2022-11-26 00:11:39 +0530

    Changes in important view

--- src/views/important.vue
+++ src/views/important.vue
@@ -1,18 +1,33 @@
 <template>
   <v-container fluid>
     <v-card-title
-      class="mb-5"
+      class="mb-n10"
       :class="
         $vuetify.breakpoint.mdAndUp ? 'text-h4 mt-n7' : 'text-h5 justify-center'
       "
-      >Important
+    >
+      <v-icon size="35" class="mr-2">mdi-star</v-icon>Important
     </v-card-title>
+    <dataTable
+      :data="
+        database.filter(function (mail) {
+          return mail.isImportant;
+        })
+      "
+      :tableTitle="'Inbox'"
+    />
   </v-container>
 </template>
 <script>
+import dataTable from "../components/table/dataTable.vue";
+import { database } from "../resources/mailDatabase";
 export default {
-  name: "important-component",
-  data: () => ({}),
-  methods: {},
+  name: "inbox-component",
+  data: () => ({ database }),
+  components: {
+    dataTable,
+  },
+
+  computed: {},
 };
 </script>
