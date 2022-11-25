ff841a0e3a139a518570f30985113f9d8a4cd504
voidrlm <varjun609@gmail.com>
2022-11-26 00:13:44 +0530

    Changes in spam view

---
+++ src/views/spam.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container fluid>
+    <v-card-title
+      class="mb-n10"
+      :class="
+        $vuetify.breakpoint.mdAndUp ? 'text-h4 mt-n7' : 'text-h5 justify-center'
+      "
+    >
+      <v-icon size="35" class="mr-2">mdi-alert-octagon-outline</v-icon>Spam
+    </v-card-title>
+    <dataTable
+      :data="
+        database.filter(function (mail) {
+          return mail.isSpam;
+        })
+      "
+      :tableTitle="'Spam'"
+    />
+  </v-container>
+</template>
+<script>
+import dataTable from "../components/table/dataTable.vue";
+import { database } from "../resources/mailDatabase";
+export default {
+  name: "inbox-component",
+  data: () => ({ database }),
+  components: {
+    dataTable,
+  },
+
+  computed: {},
+};
+</script>
