58cee802b05c59c8f6fd4a053f38e34743dec4af
voidrlm <varjun609@gmail.com>
2022-11-26 00:42:52 +0530

    Adde new mail button

---
+++ src/components/button/newMailButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-fab-transition>
+    <v-btn dark absolute buttom right fab>
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+  </v-fab-transition>
+</template>
+<script>
+export default {
+  name: "new-mail-button-component",
+  data: () => ({}),
+  components: {},
+
+  computed: {},
+};
+</script>
