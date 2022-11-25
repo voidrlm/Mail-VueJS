d5bc470abeb54ab2512419fac4a196451651986f
voidrlm <varjun609@gmail.com>
2022-11-26 00:14:14 +0530

    Added routes for views

--- src/router/routes.js
+++ src/router/routes.js
@@ -17,5 +17,29 @@ const routes = [
     meta: { authorized: true },
     component: () => import("../views/important.vue"),
   },
+  {
+    path: "/sent",
+    name: "Sent",
+    meta: { authorized: true },
+    component: () => import("../views/sent.vue"),
+  },
+  {
+    path: "/thrash",
+    name: "Thrash",
+    meta: { authorized: true },
+    component: () => import("../views/thrash.vue"),
+  },
+  {
+    path: "/spam",
+    name: "Spam",
+    meta: { authorized: true },
+    component: () => import("../views/spam.vue"),
+  },
+  {
+    path: "/draft",
+    name: "Draft",
+    meta: { authorized: true },
+    component: () => import("../views/drafts.vue"),
+  },
 ];
 export default routes;
